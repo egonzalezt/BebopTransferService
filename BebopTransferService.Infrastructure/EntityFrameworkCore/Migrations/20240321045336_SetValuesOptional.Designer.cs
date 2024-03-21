585deb1432faf872e7a6388669f33d00c967b7eb
Esteban Gonzalez Tamayo <53051438+egonzalezt@users.noreply.github.com>
2024-03-20 23:56:41 -0500

    fix(transfer): set fields optional

--- BebopTransferService.Infrastructure/EntityFrameworkCore/Migrations/20240321045336_SetValuesOptional.Designer.cs
+++ BebopTransferService.Infrastructure/EntityFrameworkCore/Migrations/20240321045336_SetValuesOptional.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BebopTransferService.Infrastructure.EntityFrameworkCore.DbContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BebopTransferService.Infrastructure.Migrations
 {
     [DbContext(typeof(BebopDbContext))]
-    partial class BebopDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240321045336_SetValuesOptional")]
+    partial class SetValuesOptional
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BebopTransferService.Infrastructure/EntityFrameworkCore/Migrations/20240321045336_SetValuesOptional.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BebopTransferService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SetValuesOptional : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "Transfers",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "UserIdentificationNumber",
+                table: "Transfers",
+                type: "bigint",
+                maxLength: 10,
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldMaxLength: 10);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserAddress",
+                table: "Transfers",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "Transfers",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "UserIdentificationNumber",
+                table: "Transfers",
+                type: "bigint",
+                maxLength: 10,
+                nullable: false,
+                defaultValue: 0L,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldMaxLength: 10,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserAddress",
+                table: "Transfers",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
